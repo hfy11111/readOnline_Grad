62725c4b16a99fe063ea4c8eb2f99a489920dacc
hfy11111 <1178805691@qq.com>
2020-12-22 20:53:49 +0800

    first commit

---
+++ readonline-api/src/main/java/com/hfy/pojo/Student.java
@@ -0,0 +1,20 @@
+package com.hfy.pojo;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@Accessors
+public class Student implements Serializable {
+    private int id;
+    private String name;
+
+    public Student(String name){
+     this.name=name;
+    }
+
+}
